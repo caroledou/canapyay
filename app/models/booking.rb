5ea0dafc85ef7ec04034d538e161aa8ae70adb05
Carole Douchy <carole.douchy@ehl.ch>
2019-02-19 14:32:33 +0100

    validation for start-end date for new bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :sofa
   belongs_to :user
   validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, format: { with: /\w{2}\/\w{2}\/20\w{2}/ }
 end
