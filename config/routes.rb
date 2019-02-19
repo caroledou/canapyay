e4705a4e3ff7a39943673e3ec66c3eed19d03184
Carole Douchy <carole.douchy@ehl.ch>
2019-02-19 11:16:27 +0100

    bookings routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/show'
-  resources :sofas, only: [:index, :new, :create]
+  resources :sofas, only: [:index, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: :show
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
