889414a6e3e1ef524c1418d8140306c1a8a6b2a9
Carole Douchy <carole.douchy@ehl.ch>
2019-02-19 11:56:15 +0100

    view show booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,27 @@
 class BookingsController < ApplicationController
   def new
-    @sofa = Sofa.find(params[:sofa_id])
-    @booking = Booking.new
+    # @sofa = Sofa.find(params[:sofa_id])
+    # @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    # @booking = Booking.find(params[:id])
+    # @booking.sofa = Sofa.find(params[:sofa_id])
+    # @booking.user = User.find(params[:user_id])
+    # if @booking.save
+    #   redirect_to booking_path
+    # else
+    #   render :new
+    # end
   end
 
   def show
     @booking = Booking.find(params[:id])
-    @booking.sofa = Sofa.find(params[:sofa_id])
-    @booking.user = User.find(params[:user_id])
-    if @booking.save
-      redirect_to booking_path
-    else
-      render :new
-    end
   end
 
-  private
+  # private
 
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
-  end
+  # def booking_params
+  #   params.require(:booking).permit()
+  # end
 end
