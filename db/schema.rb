487d3fa032a733119b1f99e1c6ed2f663d91f074
Carole Douchy <carole.douchy@ehl.ch>
2019-02-19 11:10:45 +0100

    Migrations table bookings created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_140711) do
+ActiveRecord::Schema.define(version: 2019_02_19_100555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "star_date"
+    t.date "end_date"
+    t.bigint "sofa_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sofa_id"], name: "index_bookings_on_sofa_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "sofas", force: :cascade do |t|
     t.string "name"
@@ -41,4 +52,6 @@ ActiveRecord::Schema.define(version: 2019_02_18_140711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "sofas"
+  add_foreign_key "bookings", "users"
 end
