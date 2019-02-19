56622e81a9ac076b9d749192b8660afaa58240a4
Carole Douchy <carole.douchy@ehl.ch>
2019-02-19 11:12:26 +0100

    BookingsController created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/show'
   resources :sofas, only: [:index, :new, :create]
   devise_for :users
   root to: 'pages#home'
