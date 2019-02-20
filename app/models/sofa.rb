5d8fc9bf138adfa9a58bdf142605e973cfaed623
Albert Campillo <albertcampillo@gmail.com>
2019-02-20 11:59:04 +0100

    added to Sofa model geocoded_by & after_validation

--- app/models/sofa.rb
+++ app/models/sofa.rb
@@ -2,4 +2,7 @@ class Sofa < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   validates :name, :description, :capacity, :price_per_day, :location, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
