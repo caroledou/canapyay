6a1d23652b1a46767dc7cea877cb6444035121a0
Carole Douchy <carole.douchy@ehl.ch>
2019-02-22 14:32:28 +0100

    autocomplete address in add sofa form

--- app/controllers/sofas_controller.rb
+++ app/controllers/sofas_controller.rb
@@ -38,7 +38,7 @@ class SofasController < ApplicationController
     @markers = [{
       lng: @sofa.longitude,
       lat: @sofa.latitude,
-      infoWindow: render_to_string(partial: "infowindow", locals: { howmuch: @sofa.price_per_day }),
+      infoWindow: render_to_string(partial: "infowindow", locals: { address: @sofa.address }),
       image_url: helpers.asset_url('sofa_circle.png')
     }]
   end
@@ -46,6 +46,6 @@ class SofasController < ApplicationController
   private
 
   def sofa_params
-    params.require(:sofa).permit(:name, :description, :location, :price_per_day, :capacity, :comfort, :age, :wineproof, :bed, :photo)
+    params.require(:sofa).permit(:name, :description, :location, :price_per_day, :capacity, :comfort, :age, :wineproof, :bed, :photo, :address)
   end
 end
